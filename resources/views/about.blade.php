8aa41749841d6113e3ee657fc10260ceb93f4f67
ardianti04 <ardiantisimanjuntak5@gmail.com>
2024-02-20 20:08:56 +0700

    menambahkan routes ,controller dan view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Blog | About</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3>Ardianti Simanjuntak</h3>
+    <p>ardiantisimanjuntak@gmail.com</p>
+    <img src="img/ardianti.jpeg" alt="ardianti" width="200">
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\homeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [homeController::class,'index']);
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/post', function () {
+    return view('post');
 });
